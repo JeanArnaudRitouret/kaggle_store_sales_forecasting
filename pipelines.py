8a75b9afd9bf63a934dd30526f9172452c9627f9
JeanArnaudRitouret <86613080+JeanArnaudRitouret@users.noreply.github.com>
2022-01-20 17:23:58 +0100

    first draft of preprocessing pipeline finished

--- pipelines.py
+++ pipelines.py
@@ -16,12 +16,17 @@ class PreprocessingTranformer(BaseEstimator, TransformerMixin):
         X_['date'] = pd.to_datetime(X_.date)
         X_['year'] = X_.date.dt.year
         X_['month'] = X_.date.dt.month
-        X_['week'] = X_.date.dt.week
-        #X_.day_of_week = X_.date.dt.dayofweek
+        X_['sin_month'] = np.sin(2*np.pi*X_.month/12)
+        X_['cos_month'] = np.cos(2*np.pi*X_.month/12)
+        #X_['week'] = X_.date.dt.isocalendar().week
+        X_['day_of_week'] = X_.date.dt.dayofweek
+        X_['sin_day_of_week'] = np.sin(2*np.pi*X_.day_of_week/6)
+        X_['cos_day_of_week'] = np.cos(2*np.pi*X_.day_of_week/6)
+        X_.drop(columns=['date','month','day_of_week'], inplace=True)
         return X_
 
     def get_feature_names_out(self, input_features=None):
-        input_features += ['year','month','week']
-        print(input_features)
+        input_features = ['year','sin_month','cos_month', 'sin_day_of_week', 'cos_day_of_week']
+        #input_features += [col for col in ['year','sin_month','cos_month', 'day_of_week'] if col not in input_features]
         return np.asarray(input_features, dtype=object)
 
